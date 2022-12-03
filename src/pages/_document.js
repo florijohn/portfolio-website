3851b4a9a8b781461d263d020119249df791244e
Floris <65296458+florijohn@users.noreply.github.com>
2022-12-03 21:56:17 +0100

    add GTM

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -40,8 +40,11 @@ export default class MyDocument extends Document {
           <link href="/fonts/fonts.css" rel="stylesheet"/>
           <link rel="preload" as="font" />
           <meta name="theme-color" content="#000"/>
+          
+          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WWD88KL');</script>
         </Head>
         <body>
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WWD88KL"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
           <Main />
           <NextScript />
         </body>
